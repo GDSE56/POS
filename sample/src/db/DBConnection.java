c5ee79e4b65d18197297aff46e5de4c916792143
chapachandi <chapachandi23@gmail.com>
2021-10-09 10:11:54 +0530

    db connection created

---
+++ sample/src/db/DBConnection.java
@@ -0,0 +1,25 @@
+
+package lk.ijse.thogakade.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBConnection {
+    private static DBConnection dBConnection;
+    private Connection connection;
+    private DBConnection() throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver");
+        connection=DriverManager.getConnection("jdbc:mysql://localhost:3307/ThogaKade", "root", "ijse");
+    } 
+    public Connection getConnection(){
+        return connection;
+    } 
+    public static DBConnection getInstance() throws ClassNotFoundException, SQLException{
+        if(dBConnection==null){
+            dBConnection=new DBConnection();
+        }
+        return dBConnection;
+    }
+}
