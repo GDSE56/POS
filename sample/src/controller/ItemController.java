f9b20eff59c3e07ffde39daafd75713666bd5c74
chapachandi <chapachandi23@gmail.com>
2021-10-09 10:15:38 +0530

    item controller completed

---
+++ sample/src/controller/ItemController.java
@@ -0,0 +1,75 @@
+
+package lk.ijse.thogakade.controller;
+
+import lk.ijse.thogakade.model.Item;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import lk.ijse.thogakade.db.DBConnection;
+
+
+public class ItemController {
+
+    public static boolean addItem(Item item) throws ClassNotFoundException, SQLException {
+
+        Connection connection = DBConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Item VALUES (?,?,?,?)");
+
+        pstm.setObject(1, item.getCode());
+        pstm.setObject(2, item.getDescription());
+        pstm.setObject(3, item.getUnitPrice());
+        pstm.setObject(4, item.getQtyOnHand());
+
+        int affectedRows = pstm.executeUpdate();
+
+        return affectedRows > 0;
+
+    }
+
+    public static ArrayList<Item> loadAllItems() throws ClassNotFoundException, SQLException {
+
+        Connection connection = DBConnection.getInstance().getConnection();
+        Statement stm = connection.createStatement();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Item");
+
+        ArrayList<Item> alItems = new ArrayList<>();
+
+        while (rst.next()) {
+
+            Item item = new Item();
+            item.setCode(rst.getString(1));
+            item.setDescription(rst.getString(2));
+            item.setUnitPrice(rst.getDouble(3));
+            item.setQtyOnHand(rst.getInt(4));
+
+            alItems.add(item);
+        }
+
+        return alItems;
+
+    }
+
+    public static Item searchItem(String itemCode) throws ClassNotFoundException, SQLException {
+
+        Connection connection = DBConnection.getInstance().getConnection();
+        Statement stm = connection.createStatement();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Item WHERE code='" + itemCode + "'");
+
+        if (rst.next()) {
+            Item item = new Item();
+            item.setCode(rst.getString(1));
+            item.setDescription(rst.getString(2));
+            item.setUnitPrice(rst.getDouble(3));
+            item.setQtyOnHand(rst.getInt(4));
+
+            return item;
+        } else {
+            return null;
+        }
+
+    }
+
+}
